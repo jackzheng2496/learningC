387e71072afd9946b347f56f6c62385590991b85
Jack Zheng <jackzheng2496@gmail.com>
2016-03-12 23:14:23 -0500

    Simple program that prints a histogram of character inputs

--- main.c
+++ main.c
@@ -1,25 +1,70 @@
 #include <stdio.h> 
+#include <string.h> 
+
+#define INPUT 95
+
+void fill_array(char args[], int *ptr);
+int find_max(int *ptr, int size);
 
 int main(int argc, char *argv[]) {
-	int ages[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
-	char *names[] = {"jack", "juby", "jorman", "jick"}; 
+	int i = 0; 
 
-	//Pointer to ages 
-	int *cur_age = ages; 
+	//Hard-coded number of user-inputted characters
+	int args[INPUT] = {0};
+	int *ptr = args; 
 
-	//Pointer to names which in itself, is a pointer to "jack"
-	char **cur_name = names;
-
-	//All types of pointers are 8 bytes in 64 bit systems
-	printf("%ld\t%ld\n", sizeof(cur_name), sizeof(cur_age));
-
-	int size = sizeof(names) / sizeof(cur_name);
-
-	int i = 0;
-
-	for (i = 0; i < size; i++) {
-		printf("The age of %s is %d\n", *(cur_name + i), *(cur_age + i));
+	for (i = 0; argv[i] != '\0'; i++) {
+		fill_array(argv[i], ptr);
 	}
+
+	int max_int = find_max(ptr, INPUT);
+
+	for (i = max_int; i > 0; i--) {
+		printf("%d ", i);
+
+		for (int j = 0; j < INPUT; j++) {
+			if (args[j] >= i) {
+				printf("|");
+			} 
+		}
+
+		printf("\n");
+	}
+
+	printf("  ");
+
+	for (i = 0; i < INPUT; i++) {
+		if (args[i] > 0) {
+			printf("%c", i + 32);
+		}
+	}
+
+	printf("\n");
 
 	return 0;
 }
+
+void fill_array(char args[], int *ptr) {
+	int i = 0; 
+
+	for (i = 0; args[i] != '\0'; i++) {
+		char ch = args[i]; 
+		ch = ch - 32;	//Zero index the character 
+
+		*(ptr + ch) = *(ptr + ch) + 1;
+	}
+}
+
+int find_max(int *ptr, int size) {
+	int i = 0; 
+	int max_int = 0; 
+
+	for (i = 0; i < size; i++) {
+		if (*(ptr + i) > max_int) {
+			max_int = *(ptr + i);
+		}
+	}
+
+	return max_int;
+}
+
