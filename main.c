91f4770c31836cfc7e208ec429942afe8337feb4
Jack Zheng <jackzheng2496@gmail.com>
2016-03-17 14:58:10 -0400

    typedef is pretty useful

--- main.c
+++ main.c
@@ -1,70 +1,44 @@
 #include <stdio.h> 
-#include <string.h> 
+#include <stdlib.h>
 
-#define INPUT 95
+typedef int (*operations)(int a, int b); 
 
-void fill_array(char args[], int *ptr);
-int find_max(int *ptr, int size);
+void do_some(int a, int b, operations op) {
+	int x = op(a,b); 
+	printf("%d\n", x);
+}
+
+int mul_2(int a, int b) {
+	return a * b; 
+}
+
+int add_x(int a, int b) {
+	return a + b;
+}
+
+int sub(int a, int b) {
+	return a - b;
+}
 
 int main(int argc, char *argv[]) {
-	int i = 0; 
+	int arg1 = atoi(argv[1]);
+	int arg2 = atoi(argv[2]); 
 
-	//Hard-coded number of user-inputted characters
-	int args[INPUT] = {0};
-	int *ptr = args; 
+	char *op = argv[3];
 
-	for (i = 0; argv[i] != '\0'; i++) {
-		fill_array(argv[i], ptr);
+	switch (*op) {
+		case 'm': 
+			do_some(arg1, arg2, mul_2); 
+			break;
+		case 'a': 
+			do_some(arg1, arg2, add_x); 
+			break; 
+		case 's': 
+			do_some(arg1, arg2, sub); 
+			break;
+		default: 
+			printf("Option invalid\n"); 
 	}
-
-	int max_int = find_max(ptr, INPUT);
-
-	for (i = max_int; i > 0; i--) {
-		printf("%d ", i);
-
-		for (int j = 0; j < INPUT; j++) {
-			if (args[j] >= i) {
-				printf("|");
-			} 
-		}
-
-		printf("\n");
-	}
-
-	printf("  ");
-
-	for (i = 0; i < INPUT; i++) {
-		if (args[i] > 0) {
-			printf("%c", i + 32);
-		}
-	}
-
-	printf("\n");
 
 	return 0;
 }
-
-void fill_array(char args[], int *ptr) {
-	int i = 0; 
-
-	for (i = 0; args[i] != '\0'; i++) {
-		char ch = args[i]; 
-		ch = ch - 32;	//Zero index the character 
-
-		*(ptr + ch) = *(ptr + ch) + 1;
-	}
-}
-
-int find_max(int *ptr, int size) {
-	int i = 0; 
-	int max_int = 0; 
-
-	for (i = 0; i < size; i++) {
-		if (*(ptr + i) > max_int) {
-			max_int = *(ptr + i);
-		}
-	}
-
-	return max_int;
-}
-
